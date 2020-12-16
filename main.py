8ac8d299f9f42effe7e9b1a64a7c2e7d30960279
위제현/AI서비스개발스쿼드 <wjh6083@sk.com>
2020-12-16 22:54:19 +0900

    20201216

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import os
 import tarfile
 import urllib.request
+import numpy as np
+from zlib import crc32
+
+
+
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def test_set_check(identifier, test_ratio):
+    #python2와의 호환을 위한 비트연산
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
 
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
